6f42887b15d18ba4ea1cdb9c3460360f64991166
Roy Oltmans <roy@oltmans.nl>
2025-06-09 01:27:10 +0200

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -1,1 +1,1 @@
-docker run -dit --name anova_server -p 8000:8000 -p 8090:8080 anova:0.0.1
+docker run -dit --name anova_server -p 8000:8000 -p 8090:8080 -e BLE_PROXY_URL=http://rpi-ble-proxy.local.int:5000 anova-server
